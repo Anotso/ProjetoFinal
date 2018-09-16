23db6a5912682c689776c1df0cc28c05f0e8cbf7
fabio <fabio@192.168.1.2>
2018-09-16 16:12:32 -0300

    Fábio - add arquivos, primeira edição 16/09/2018

--- web/recursos/js/js.js
+++ web/recursos/js/js.js
@@ -1,12 +1,33 @@
 
-/*funçao dotopp*/
-var topo = function() {
-    window.scrollTo(0, 0);
-};
-var d = new Date();
-document.getElementById("Date").innerHTML = d;
+// FUNÇAO CHAMA 
+
+
+// Obtém o campo de entrada
+
+var input = document.getElementById("senha");
+var text = document.getElementById("text12");
+
+
+// Quando o usuário pressiona qualquer tecla no teclado, execute a função
+
+document.addEventListener('keyup', function( event ) {
+
+  /// Se "caps lock" for pressionado, exibir o texto de aviso
+  if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
 
 
 
-
-
+//funçao mostrar senha
+function mostrar() {
+  var x = document.getElementById("senha");
+  if (x.type === "password") {
+      x.type = "text";
+  } else {
+      x.type = "password";
+  }
+}
